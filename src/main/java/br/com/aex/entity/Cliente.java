f41d65e48d1efdf82b9ec598285fa0765e51c647
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-19 17:36:33 -0300

    feat: relacionamento cliente x pedido
    Usando mappedBy para evitar a criação de uma tabela extra de relacionamento.
    O "cliente" do mappedBy indica o nome do atributo Cliente na classe Pedido.
    
    Definindo fetchType.LAZY para nao fazer um select no Cliente sempre que recuperar um Pedido.
    O atributo name da annotation @JoinColumn é para indicar o nome da coluna que armazenará o id de cliente na tabela Pedido.

--- src/main/java/br/com/aex/entity/Cliente.java
+++ src/main/java/br/com/aex/entity/Cliente.java
@@ -1,17 +1,24 @@
 package br.com.aex.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
 public class Cliente {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String nome;
     private String telefone;
 
+    @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
+    private List<Pedido> pedido;
+    
 }
---
+++ src/main/java/br/com/aex/entity/Pedido.java
@@ -0,0 +1,29 @@
+package br.com.aex.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private LocalDateTime dataCriacao;
+    private BigDecimal valor;
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id", foreignKey = @ForeignKey(name = "fk_pedido_cliente"))
+    private Cliente cliente;
+
+}
