2e5c72ccff38ad4ed647f05305ee8dfd61a70231
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-18 21:15:49 -0300

    feat: iniciando projeto

---
+++ src/main/java/br/com/aex/ProjetoAplicadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.aex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAplicadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAplicadoApplication.class, args);
+	}
+
+}
