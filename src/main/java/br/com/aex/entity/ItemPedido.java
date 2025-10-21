09f56d2c6f2cac3f143110b7e8555d19857896d7
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-21 08:05:48 -0300

    feat: mapeando ItemPedido

---
+++ src/main/java/br/com/aex/entity/ItemPedido.java
@@ -0,0 +1,28 @@
+package br.com.aex.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import java.math.BigDecimal;
+
+@Entity(name = "item_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer quantidade;
+    private BigDecimal precoUnitario;
+
+    @ManyToOne
+    private Pedido pedido;
+
+    @ManyToOne
+    private Produto produto;
+
+
+}
