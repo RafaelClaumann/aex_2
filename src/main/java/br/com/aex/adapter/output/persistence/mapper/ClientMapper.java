62c1e3565eb18408a35cdafc17fa590d2c71f8e7
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-22 07:34:30 -0300

    feat: ajustando mapstruct para converted entity em model

--- src/main/java/br/com/aex/adapter/output/persistence/mapper/ClientMapper.java
+++ src/main/java/br/com/aex/adapter/output/persistence/mapper/ClientMapper.java
@@ -3,8 +3,9 @@ package br.com.aex.adapter.output.persistence.mapper;
 import br.com.aex.domain.models.ClienteModel;
 import br.com.aex.adapter.output.persistence.entities.Cliente;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ClientMapper {
 
     ClienteModel toClientModel(final Cliente source);
--- src/main/java/br/com/aex/domain/usecase/GetClientPortImpl.java
+++ src/main/java/br/com/aex/domain/usecase/GetClientPortImpl.java
@@ -1,18 +1,24 @@
 package br.com.aex.domain.usecase;
 
+import br.com.aex.domain.models.ClienteModel;
 import br.com.aex.domain.ports.input.GetClientPort;
+import br.com.aex.domain.ports.output.ClientRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GetClientPortImpl implements GetClientPort {
 
-    // private final RepositoryPort repositoryPort
+    private final ClientRepository clientRepository;
+
+    public GetClientPortImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
     @Override
     public String getClient() {
-        System.out.println("invocando repositoryPort");
-        System.out.println("convertendo o cliente");
-        return "retornando o cliente";
+        ClienteModel clientById = clientRepository.getClientById(5L);
+        System.out.println("Cliente Encontrado: " + clientById);
+        return "retornando o cliente com id 5";
     }
 
 }
