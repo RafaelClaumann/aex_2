0e4108a906033d664b959147c8c7a6b246440e6e
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-22 22:31:11 -0300

    feat: adicionando handler para ClientNotFoundException

--- src/main/java/br/com/aex/controller/v1/ClientController.java
+++ src/main/java/br/com/aex/controller/v1/ClientController.java
@@ -2,10 +2,15 @@ package br.com.aex.controller.v1;
 
 import br.com.aex.entity.Cliente;
 import br.com.aex.service.ClientService;
+import br.com.aex.service.exceptions.ClientNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +27,12 @@ public class ClientController {
     public ResponseEntity<Cliente> getClient(@PathVariable final Long id) {
         Cliente client = clientService.getClient(id);
         return ResponseEntity.ok(client);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ProblemDetail handleClientNotFound(final ClientNotFoundException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
 }
