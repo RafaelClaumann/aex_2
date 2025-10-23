11ff525a898f730e0ab9ff691364f90d0075fa1c
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-23 19:41:02 -0300

    refact: excluindo exception nao utilizada

--- src/main/java/br/com/aex/service/exceptions/ClientNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package br.com.aex.service.exceptions;
-
-public class ClientNotFoundException extends RuntimeException {
-
-    public ClientNotFoundException(final Long id) {
-        super(String.format("Client with id %s not found", id));
-    }
-
-}
