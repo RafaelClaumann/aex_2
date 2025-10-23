0d2ea0d8cafd63c6a6dc8e9255edd3cf3150ca8c
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-22 22:42:21 -0300

    feat: salvando cliente no banco

--- src/main/java/br/com/aex/service/ClientService.java
+++ src/main/java/br/com/aex/service/ClientService.java
@@ -1,10 +1,12 @@
 package br.com.aex.service;
 
+import br.com.aex.controller.v1.ClientDtoV1;
 import br.com.aex.entity.Cliente;
 import br.com.aex.repository.ClientRepository;
 import br.com.aex.service.exceptions.ClientNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +21,16 @@ public class ClientService {
     public Cliente getClient(final Long id) {
         Optional<Cliente> cliente = clientRepository.findById(id);
         return cliente.orElseThrow(() -> new ClientNotFoundException(id));
+    }
+
+    public void SaveClient(final ClientDtoV1 cliente) {
+        Cliente clienteEntity = Cliente.builder()
+                .nome(cliente.nome())
+                .telefone(cliente.telefone())
+                .pedido(List.of())
+                .build();
+
+        clientRepository.save(clienteEntity);
     }
 
 }
