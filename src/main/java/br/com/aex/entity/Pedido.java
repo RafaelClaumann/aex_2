bcab819d870725d3775f00e26d7503847543a79c
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-23 22:52:42 -0300

    feat: adicionando anotações lombok no Pedido

--- src/main/java/br/com/aex/entity/Pedido.java
+++ src/main/java/br/com/aex/entity/Pedido.java
@@ -8,10 +8,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "pedido")
 public class Pedido {
 
