a32d4b5dee04b7c750ce507f823dfc4b5414980a
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-24 08:08:22 -0300

    feat: endpoint listando orders dos clientes
    O endpoint foi criado no controller de cliente ClientController

---
+++ src/main/java/br/com/aex/api/dto/client/ClientResponseDtoV1.java
@@ -0,0 +1,20 @@
+package br.com.aex.api.dto.client;
+
+import br.com.aex.api.dto.order.OrderResponseDtoV1;
+import br.com.aex.entity.Cliente;
+
+import java.util.List;
+
+public record ClientResponseDtoV1(
+        ClientDtoV1 client,
+        List<OrderResponseDtoV1> orders
+) {
+
+    public static ClientResponseDtoV1 from(final Cliente client) {
+        final ClientDtoV1 clientDto = new ClientDtoV1(client.getNome(), client.getTelefone());
+        final List<OrderResponseDtoV1> ordersDtoList = client.getPedido().stream().map(OrderResponseDtoV1::fromEntity).toList();
+
+        return new ClientResponseDtoV1(clientDto, ordersDtoList);
+    }
+
+}
