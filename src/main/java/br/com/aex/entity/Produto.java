087e96cc0a853b67f4c8fba86e64914b6ff1a443
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-25 11:32:35 -0300

    feat: adicionando anotações lombok ao Produto

--- src/main/java/br/com/aex/entity/Produto.java
+++ src/main/java/br/com/aex/entity/Produto.java
@@ -5,9 +5,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "produto")
 public class Produto {
 
