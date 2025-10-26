93f026fc1f03f13177da8f57581c709604e84151
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-26 19:38:08 -0300

    feat: método auxiliar CaregoriaEnum

--- src/main/java/br/com/aex/model/CategoriaEnum.java
+++ src/main/java/br/com/aex/model/CategoriaEnum.java
@@ -1,5 +1,6 @@
 package br.com.aex.model;
 
+import br.com.aex.service.exception.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -15,5 +16,15 @@ public enum CategoriaEnum {
     DESCONHECIDA("Desconhecida");
 
     private final String descricao;
+
+    public static CategoriaEnum from(final String categoria) {
+        for (final CategoriaEnum categoriaEnum : CategoriaEnum.values()) {
+            if (categoriaEnum.getDescricao().equals(categoria)) {
+                return categoriaEnum;
+            }
+        }
+
+        throw new ResourceNotFoundException("Categoria inexistente: " + categoria, CategoriaEnum.class.getSimpleName());
+    }
 
 }
