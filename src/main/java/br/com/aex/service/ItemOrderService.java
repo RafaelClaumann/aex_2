31ef059d5b84f6d2e85ad284acec11446bd083e8
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-26 18:01:24 -0300

    refact: removendo POST itemOrder
    Olhando para o banco de dados ess elemento deve ser criado internamente juntando informações do Cliente e Produto

--- src/main/java/br/com/aex/service/ItemOrderService.java
+++ src/main/java/br/com/aex/service/ItemOrderService.java
@@ -1,9 +1,6 @@
 package br.com.aex.service;
 
-import br.com.aex.api.dto.item_order.ItemOrderDtoV1;
 import br.com.aex.entity.ItemPedido;
-import br.com.aex.entity.Pedido;
-import br.com.aex.entity.Produto;
 import br.com.aex.repository.ItemOrderRepository;
 import br.com.aex.service.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
@@ -15,13 +12,9 @@ import java.util.Optional;
 public class ItemOrderService {
 
     private final ItemOrderRepository itemOrderRepository;
-    private final OrderService orderService;
-    private final ProductService productService;
 
-    public ItemOrderService(ItemOrderRepository itemOrderRepository, OrderService orderService, ProductService productService) {
+    public ItemOrderService(ItemOrderRepository itemOrderRepository) {
         this.itemOrderRepository = itemOrderRepository;
-        this.orderService = orderService;
-        this.productService = productService;
     }
 
     public ItemPedido getItemOrder(final Long id) {
@@ -30,20 +23,6 @@ public class ItemOrderService {
                 "ItemOrder não encontrado: " + id,
                 this.getClass().getSimpleName()
         ));
-    }
-
-    public ItemPedido saveItemOrder(final ItemOrderDtoV1 itemOrderDto) {
-        final Pedido order = orderService.getOrder(itemOrderDto.orderId());
-        final Produto product = productService.getProduct(itemOrderDto.productId());
-
-        final ItemPedido itemOrder = ItemPedido.builder()
-                .quantidade(itemOrderDto.quantity())
-                .precoUnitario(itemOrderDto.piecePrice())
-                .pedido(order)
-                .produto(product)
-                .build();
-
-        return itemOrderRepository.save(itemOrder);
     }
 
     public List<ItemPedido> saveAll(final List<ItemPedido> itemOrders) {
