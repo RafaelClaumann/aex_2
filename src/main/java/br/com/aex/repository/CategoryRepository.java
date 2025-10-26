07bdaf867526938de297288e6c3e98e56a9a364f
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-10-26 19:38:25 -0300

    feat: buscas por nome de categoria no repository

--- src/main/java/br/com/aex/repository/CategoryRepository.java
+++ src/main/java/br/com/aex/repository/CategoryRepository.java
@@ -3,5 +3,10 @@ package br.com.aex.repository;
 import br.com.aex.entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Categoria, Long> {
+
+    Optional<Categoria> findCategoryByNome(final String nome);
+
 }
