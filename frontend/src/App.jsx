daabb1b1b0207561aeaf10e2ed20a6b9741ea570
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-11-08 21:56:01 -0300

    feat:  listagem de  produtos em tabela

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import api from "./services/Api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/v1/product")
+      .then((response) => {
+        const mappedProducts = response.data.map((product) => {
+          return {
+            id: product.id,
+            name: product.nome,
+            description: product.descricao,
+            price: product.preco_venda,
+            category: product.nome_categoria,
+            quantity: 0,
+          };
+        });
+        setProducts(mappedProducts);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Produtos Disponíveis</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <table>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>{product.description}</td>
+                <td>{product.price}</td>
+                <td>{product.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
