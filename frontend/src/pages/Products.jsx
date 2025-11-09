72d885f178a2e57916eb70ff52e868c4a42b0efe
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2025-11-08 22:16:57 -0300

    lint: aumentando length das linhas no prettier

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -30,9 +30,7 @@ function Products() {
     const product = productsList.find((product) => product.id === id);
     product.quantity = product.quantity <= 999 ? product.quantity + 1 : 0;
     setProducts(productsList);
-    console.log(
-      `increment_quantity | name: ${product.name} | id: ${product.id} | quantity: ${product.quantity}`
-    );
+    console.log(`increment_quantity | name: ${product.name} | id: ${product.id} | quantity: ${product.quantity}`);
   }
 
   function decrementQuantity(id) {
@@ -40,9 +38,7 @@ function Products() {
     const product = productsList.find((product) => product.id === id);
     product.quantity = product.quantity > 0 ? product.quantity - 1 : 0;
     setProducts(productsList);
-    console.log(
-      `decrement_quantity | name: ${product.name} | id: ${product.id} | quantity: ${product.quantity}`
-    );
+    console.log(`decrement_quantity | name: ${product.name} | id: ${product.id} | quantity: ${product.quantity}`);
   }
 
   return (
